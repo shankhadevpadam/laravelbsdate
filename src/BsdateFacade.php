e751caba64eb5f13ba82a568b26f6066b0461e67
shankhadevpadam <shankhadev123@gmail.com>
2016-12-31 15:17:50 +0545

    Project Added

---
+++ src/BsdateFacade.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Shankhadev\Bsdate;
+
+use Illuminate\Support\Facades\Facade;
+
+class BsdateFacade extends Facade 
+{
+	protected static function getFacadeAccessor() { return 'bsdate'; }
+}
---
+++ src/BsdateServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Shankhadev\Bsdate;
+
+use Illuminate\Support\ServiceProvider;
+
+class BsdateServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Shankhadev\Bsdate\BsdateController');
+        $this->app->bind('bsdate', function() {
+            return new \Shankhadev\Bsdate\BsdateController;
+        });
+    }
+}
